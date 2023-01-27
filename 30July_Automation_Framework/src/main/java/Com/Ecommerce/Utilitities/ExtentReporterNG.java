303a900c5a113dbd7a63f79fbf399dea594586ba
pratikkakad <pratikkakad216@gmail.com>
2023-01-27 22:16:50 +0530

    framework

---
+++ 30July_Automation_Framework/src/main/java/Com/Ecommerce/Utilitities/ExtentReporterNG.java
@@ -0,0 +1,34 @@
+package Com.Ecommerce.Utilitities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
+
+public class ExtentReporterNG {
+	static ExtentReports extent;
+	
+	public static ExtentReports getReportObject()	
+	{	
+        String Timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+    	String Name = "ExtentReport - "+Timestamp;
+        String path = System.getProperty("user.dir")+"/Reports/"+Name+" "+".html";
+		ExtentHtmlReporter reporter = new ExtentHtmlReporter(path);
+		reporter.config().setReportName("30July Framework Extent Report");
+		reporter.config().setDocumentTitle("(30July Automation Execution Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("QA Automation Engineer", "Ranjeet Kendre");
+		extent.setSystemInfo("Suite name", "Regression");
+
+		return extent;
+		
+	}
+	   
+	 
+	 
+}
